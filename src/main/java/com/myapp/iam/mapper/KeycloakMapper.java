31526cdd8f30e41dc72e16850773ade27bc339d3
Ivan Semenov <semenovivan94@gmail.com>
2022-11-01 18:52:53 +0400

    Temporarily disable DB dependencies, added Mapstruct classes

---
+++ src/main/java/com/myapp/iam/mapper/KeycloakMapper.java
@@ -0,0 +1,28 @@
+package com.myapp.iam.mapper;
+
+import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.mapstruct.*;
+
+import java.util.*;
+
+@Mapper(componentModel = "spring")
+public interface KeycloakMapper {
+
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "username", source = "username")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "enabled", constant = "true")
+    UserRepresentation toUserRepresentation(String username, String email);
+
+    @BeanMapping(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    @Mapping(target = "type", constant = "password")
+    @Mapping(target = "temporary", source = "temporary")
+    @Mapping(target = "value", source = "newPassword")
+    CredentialRepresentation toCredentialRepresentation(String newPassword, boolean temporary);
+
+    @AfterMapping
+    default void fillOtherFields(@MappingTarget UserRepresentation userRepresentation) {
+        userRepresentation.setAttributes(Map.of("userId", Collections.singletonList(UUID.randomUUID().toString())));
+    }
+}
