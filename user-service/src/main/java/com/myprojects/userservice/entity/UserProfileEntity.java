035a8d8faf6df456ae40be039d7640550e2dc737
Ivan Semenov <semenovivan94@gmail.com>
2024-05-23 12:46:24 +0400

    Fix entity persisting

--- user-service/src/main/java/com/myprojects/userservice/entity/UserProfileEntity.java
+++ user-service/src/main/java/com/myprojects/userservice/entity/UserProfileEntity.java
@@ -2,22 +2,34 @@ package com.myprojects.userservice.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.With;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Entity
-@Table(name = "user_profiles")
+@Table(name = "user_profile")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class UserProfileEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @With
     private String keycloakId;
 
     private String firstName;
@@ -29,7 +41,4 @@ public class UserProfileEntity {
 
     @CreatedDate
     private LocalDateTime creationDate;
-
-    public UserProfileEntity() {
-    }
 }
--- user-service/src/main/resources/db/migration/V1__addUserProfileTable.sql
+++ user-service/src/main/resources/db/migration/V1__addUserProfileTable.sql
@@ -1,7 +1,7 @@
-CREATE TABLE user_profiles
+CREATE TABLE user_profile
 (
-    id                 UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    keycloak_user_id   VARCHAR(36),
+    id                 UUID PRIMARY KEY,
+    keycloak_id        VARCHAR(36) NOT NULL,
     first_name         VARCHAR(128) NOT NULL,
     last_name          VARCHAR(128) NOT NULL,
     email              VARCHAR(128) NOT NULL,
